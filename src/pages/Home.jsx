33576c71f87be3c7c34c1287b7786249f0713a59
hkshakib <humayunkibriashakib@gmail.com>
2023-06-28 10:35:25 +0600

    fix: Fix Home title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,12 @@ import { NavLink } from 'react-router-dom';
 
 function Home() {
   return (
-    <div className='flex basis-[100%] justify-center items-center'>
+    <div className='flex flex-col basis-[100%] justify-center items-center'>
+      <h1 className='uppercase text-white text-bold mb-[20px]'>WELCOME TO FUN BOX</h1>
       <div className='flex flex-col bg-slate-400 text-black border border-white h-[400px] w-[400px]'>
         <div className='flex basis-[50%]'>
           <div className='flex border border-white flex-1 justify-center items-center hover:bg-lime-900'>
-            <NavLink to="/tic-tac-toe" className="uppercase font-mono text-[16px]" >TIC TAC TOE</NavLink>
+            <NavLink to="/calculator" className="uppercase font-mono text-[16px]">CALCULATOR</NavLink>
           </div>
           <div className='flex border border-white flex-1 justify-center items-center hover:bg-lime-900'>
             <NavLink to="/to-do" className="uppercase font-mono text-[16px]">TO DO LIST</NavLink>
@@ -24,7 +25,7 @@ function Home() {
             <NavLink to="/memory-game" className="uppercase font-mono text-[16px]">MEMORY GAME</NavLink>
           </div>
           <div className='flex border border-white flex-1 justify-center items-center hover:bg-lime-900'>
-            <NavLink to="/calculator" className="uppercase font-mono text-[16px]">CALCULATOR</NavLink>
+            <NavLink to="/tic-tac-toe" className="uppercase font-mono text-[16px]" >TIC TAC TOE</NavLink>
           </div>
         </div>
       </div>
